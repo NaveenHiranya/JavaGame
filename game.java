0dd538b682fd85275cf1d16fe22455a9e8392218
Naveen Hiranya <naveenhiranya999@gmail.com>
2026-06-20 15:50:05 +0530

    initial commit

---
+++ game.java
@@ -0,0 +1,203 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+
+// ================= PLAYER =================
+class Player {
+
+    int x;
+    int y;
+    String position;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.position = "l";
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.BLUE);
+
+        if ("r".equals(position)) {
+
+            g.fillRect(x, y, 30, 30);
+            g.fillRect(x, y + 30, 30, 30);
+            g.fillRect(x, y - 30, 30, 30);
+            g.fillRect(x - 30, y + 30, 30, 30);
+            g.fillRect(x - 30, y - 30, 30, 30);
+            g.fillRect(x + 30, y, 30, 30);
+
+        } else if ("d".equals(position)) {
+
+            g.fillRect(x, y, 30, 30);
+            g.fillRect(x, y + 30, 30, 30);
+            g.fillRect(x + 30, y - 30, 30, 30);
+            g.fillRect(x - 30, y, 30, 30);
+            g.fillRect(x - 30, y - 30, 30, 30);
+            g.fillRect(x + 30, y, 30, 30);
+
+        } else if ("u".equals(position)) {
+
+            g.fillRect(x, y, 30, 30);
+            g.fillRect(x, y - 30, 30, 30);
+            g.fillRect(x - 30, y + 30, 30, 30);
+            g.fillRect(x - 30, y, 30, 30);
+            g.fillRect(x + 30, y + 30, 30, 30);
+            g.fillRect(x + 30, y, 30, 30);
+
+        } else { // LEFT
+
+            g.fillRect(x, y, 30, 30);
+            g.fillRect(x, y + 30, 30, 30);
+            g.fillRect(x, y - 30, 30, 30);
+            g.fillRect(x - 30, y, 30, 30);
+            g.fillRect(x + 30, y + 30, 30, 30);
+            g.fillRect(x + 30, y - 30, 30, 30);
+        }
+    }
+
+    public void moveLeft() {
+        x -= 10;
+        position = "l";
+    }
+
+    public void moveRight() {
+        x += 10;
+        position = "r";
+    }
+
+    public void moveUp() {
+        y -= 10;
+        position = "u";
+    }
+
+    public void moveDown() {
+        y += 10;
+        position = "d";
+    }
+}
+
+// ================= BULLET =================
+class Bullet {
+
+    int x;
+    int y;
+    int dx;
+    int dy;
+
+    public Bullet(int x, int y, int dx, int dy) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public void move() {
+        x += dx;
+        y += dy;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.RED);
+
+        if (dx == 0) {
+            g.fillRect(x, y, 5, 10); // vertical
+        } else {
+            g.fillRect(x, y, 10, 5); // horizontal
+        }
+    }
+}
+
+// ================= GAME =================
+public class game extends JPanel implements KeyListener {
+
+    Player player = new Player(100, 100);
+    ArrayList<Bullet> bullets = new ArrayList<>();
+
+    public game() {
+        setFocusable(true);
+        addKeyListener(this);
+
+        // Game loop
+        Timer timer = new Timer(30, e -> {
+
+            for (Bullet b : bullets) {
+                b.move();
+            }
+
+            repaint();
+        });
+
+        timer.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        player.draw(g);
+
+        for (Bullet b : bullets) {
+            b.draw(g);
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        // MOVEMENT
+        if (e.getKeyCode() == KeyEvent.VK_LEFT)
+            player.moveLeft();
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+            player.moveRight();
+
+        if (e.getKeyCode() == KeyEvent.VK_UP)
+            player.moveUp();
+
+        if (e.getKeyCode() == KeyEvent.VK_DOWN)
+            player.moveDown();
+
+        // FIRE
+        if (e.getKeyCode() == KeyEvent.VK_X) {
+
+            if ("l".equals(player.position)) {
+                bullets.add(new Bullet(player.x - 30, player.y + 15, -10, 0));
+            }
+            if ("r".equals(player.position)) {
+                bullets.add(new Bullet(player.x + 60, player.y + 15, 10, 0));
+            }
+            if ("u".equals(player.position)) {
+                bullets.add(new Bullet(player.x + 15, player.y - 30, 0, -10));
+            }
+            if ("d".equals(player.position)) {
+                bullets.add(new Bullet(player.x + 15, player.y + 60, 0, 10));
+            }
+        }
+
+        repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("2D Shooter");
+
+        game g = new game();
+
+        frame.add(g);
+        frame.setSize(900, 900);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        g.requestFocusInWindow();
+    }
+}
